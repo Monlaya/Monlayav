d04f3878bbe39b13bc8c5c99b70d06da6ccbdd99
Monlaya <98200737+Monlaya@users.noreply.github.com>
2022-12-04 15:40:38 +0700

    Add files via upload

---
+++ src/pages/Home.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+  <Navbar />
+  <section class="home">
+
+    <div class="image">
+        <img src="images\1.jpg" alt="">
+    </div>
+
+    <div class="content">
+        <h3>Hello</h3>
+        <span>Monlaya Kunpratum</span>
+    </div>
+
+</section>
+    </div>
+</template>
+
+<script>
+import Navbar from '../components/Navbar.vue';
+export default {
+    components: { Navbar }
+}
+</script>
+
+<style>
+
+</style>
